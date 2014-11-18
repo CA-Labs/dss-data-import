0bf5320c9ac4f5e7fd219f2573a0016a351ee1e3
Jordi Aranda <jordi.aranda@gmail.com>
2014-11-18 17:40:18 +0100

    Adding dummy test example

---
+++ src/test/scala/DummySpec.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FlatSpec
+
+/**
+ * Created by Jordi Aranda
+ * <jordi.aranda@bsc.es>
+ * 18/11/14.
+ */
+class DummySpec extends FlatSpec {
+
+  "A dummy test" should "always be true" in {
+    assert(true)
+  }
+
+}
